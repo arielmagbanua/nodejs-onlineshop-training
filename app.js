91f4f9bd8c249b69e8ac43158df99f02fa873ae1
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-04-15 22:13:21 +0800

    Initial work during training.

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', (req, res, next) => {
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    res.send(
+    `<form action="/product" method="POST">
+        <input type="text" name="title">
+        <button type="submit">Add</button>
+    </form>`);
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body.title);
+    
+    res.redirect('/');
+});
+
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Index</h2>');
+});
+
+app.listen(3000);
