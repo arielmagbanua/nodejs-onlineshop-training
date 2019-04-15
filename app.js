1a975be0242c299ca3921d8c26903c034dae1d81
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-04-15 22:24:44 +0800

    Added routes.

--- app.js
+++ app.js
@@ -2,29 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    next();
-});
+app.use(adminRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send(
-    `<form action="/product" method="POST">
-        <input type="text" name="title">
-        <button type="submit">Add</button>
-    </form>`);
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body.title);
-    
-    res.redirect('/');
-});
-
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Index</h2>');
-});
+app.use(shopRoutes);
 
 app.listen(3000);
