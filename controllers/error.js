19f2b70e979e6e049df67f734fc18ec2987e5198
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-09-08 00:23:47 +0800

    Applied the changes made on CRUD operations of products in cart.

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404 = (req, res, next) => {
-    res.render('404', { path: '/', pageTitle: 'Page Not Foumd.' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
