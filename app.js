e7a53c802586b7de0cd7fd70c8deb540c0c4356c
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2019-09-08 15:55:43 +0800

    Implemented sequelize

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 // parse request body
 app.use(bodyParser.urlencoded({extended: false}));
@@ -22,4 +23,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(9000);
+sequelize.sync()
+    .then((result) => {
+        // console.log(result);
+        app.listen(9000);
+    })
+    .catch((error) => console.log(error));
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+    'node-complete', 
+    'root', 
+    '',
+    { dialect: 'mysql', host: 'localhost'});
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: ''
-});
-
-module.exports = pool.promise();
+module.exports = sequelize;
